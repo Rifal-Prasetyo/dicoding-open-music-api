751117938499c7c925954f3afbd7d3f46339c619
rifal-prasetyo <hrdsdms765@gmail.com>
2025-02-28 18:35:31 +0700

    fix contraint playlist_songs

--- migrations/1740058207544_playlist-songs.js
+++ migrations/1740058207544_playlist-songs.js
@@ -28,7 +28,7 @@ exports.up = (pgm) => {
   pgm.addConstraint('playlist_songs', 'fk_playlist_songs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
 
   // memberikan contraints foreign key pada song_id terhadap kolom id dari songs
-  pgm.addConstraint('playlist_songs', 'fk_playlist_songs.song_id_songs.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_songs', 'fk_playlist_songs.song_id_songs.id', 'FOREIGN KEY(playlist_id) REFERENCES songs(id) ON DELETE CASCADE');
 };
 
 /**
